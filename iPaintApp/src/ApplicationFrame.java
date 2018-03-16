9921ffee275627b844e8186aba356da2e2ddce0e
Aleisha <nelson.aleisha@gmail.com>
2018-03-15 20:54:23 -0700

    Aleishaa's Changes

--- iPaintApp/src/ApplicationFrame.java
+++ iPaintApp/src/ApplicationFrame.java
@@ -1,3 +1,4 @@
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -8,8 +9,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
+
 import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -20,59 +24,34 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.FlowLayout;
+import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Dimension;
 
 public class ApplicationFrame extends JFrame
 {
 	private JLabel stausLabel; //label display mouse coordinates
-    private JButton undo, redo, clear, selectShape; // buttons to undo, redo last shape drawn and to clear the canvas
-    private JButton jbRect, jbEllipse, jbLine, jbText, jbSquare, jbCircle, jbTriangle;  //buttons for selecting which shape the user wants to draw
-    private JComboBox colors; //combobox with color options 
+    private JButton undo, redo, clear;// selectShape; // buttons to undo, redo last shape drawn and to clear the canvas
+    private JButton jbRect, jbEllipse, jbLine, jbText, jbSquare, jbCircle, jbTriangle, jbSave, jbColorChooser;  //buttons for selecting which shape the user wants to draw
     private JCheckBox fillCheckBox; //checkbox to select whether or not to fill a shape with color
-    private JLabel colorLabel;
-    private JCheckBox animateCheckBox;
-    private JButton btnStart;
-    private JButton btnStop;
-    private JComboBox font;
-    
-        
+    private Color c;  //color variable to hold color value for shape fill    
     private JPanel toolboxPanel; //panel to hold buttons for drawing shapes and selecting colors
     private JPanel toolboxPadding; //adds padding around the toolbox panel
-    public static CanvasPanel canvas; //canvas panel for drawing shapes
-    
+    private CanvasPanel canvas; //canvas panel for drawing shapes
     private Icons JBicons;
-    
-  //array holding the different color options for filling a shape
-    private Color colorsArray[]= {Color.BLACK , Color.WHITE , Color.MAGENTA , Color.BLUE , Color.CYAN , Color.GREEN, Color.YELLOW, 
-    		Color.ORANGE , Color.RED , Color.PINK , Color.darkGray , Color.GRAY , 
-         Color.lightGray };
-    
-    //array of strings containing color options for JComboBox colors
-    private String colorMenu[]=
-    {"Black", "White", "Magenta", "Blue","Cyan", "Green", "Yellow", 
-    		"Green", "Orange", "Red", "Pink", "Light Gray","Dark Gray","Gray"};
-    
-    private String fontSize[] = {"10", "12", "14", "18", "20", "22", "26", "28", "30"};
-
+   
     //array of strings containing shape options for JComboBox shapes
     private String shapeOptions[]={"Line","Rectangle","Ellipse", "Text", "Circle", "Square", "Triangle"};
-    
-    /**
-     * CONSTRUCTOR for JFrame/Application
-     * Creates the canvas JPanel object for drawing shapes/textfields, adds event handlers, and initializes variables
-     */
-    
+
+//CONSTRUCTOR For Paint Application Frame    
     public ApplicationFrame()
     {
-    		
-        super("iPaint Application"); 
+        super("iPaint Application");  //Set label at the top of the application frame
             
         JLabel statusLabel = new JLabel( "" ); //create an empty JLabel to pass in to the canvas Panel
         
         canvas = new CanvasPanel(statusLabel); //creates the canvas panel and passes in the statusLabel param
         canvas.setBorder(BorderFactory.createLineBorder(Color.GRAY , 15)); 
-        //canvas.setLayout(null);
        
         Icons JBicons = new Icons();   //instantiates Icons object which defines images to use for icons
           
@@ -84,32 +63,28 @@ public class ApplicationFrame extends JFrame
         clear = new JButton("Clear");
         clear.setIcon(JBicons.clearII);
         jbRect = new JButton("Rectangle");
-        jbRect.setIcon(JBicons.squareII);
+        jbRect.setIcon(JBicons.rectangleII);
+        jbSquare = new JButton("Square");
+        jbSquare.setIcon(JBicons.squareII);
         jbEllipse = new JButton("Ellipse");
-        jbEllipse.setIcon(JBicons.circleII);
+        jbEllipse.setIcon(JBicons.ellipseII);
+        jbCircle = new JButton("Circle");
+        jbCircle.setIcon(JBicons.circleII);
         jbLine = new JButton("Line");
         jbLine.setIcon(JBicons.lineII);
         jbText = new JButton("Text");
         jbText.setIcon(JBicons.textII);
-        selectShape = new JButton("Recolor Shape");
-        jbCircle = new JButton("Circle");
-        jbSquare = new JButton("Square");
+        //selectShape = new JButton("Recolor Shape");
         	jbTriangle = new JButton("Triangle");
+        	jbTriangle.setIcon(JBicons.triangleII);
+        	jbSave = new JButton("Save");
+        	jbSave.setIcon(JBicons.saveII);
         	
-        	btnStart = new JButton("Start");
-        	btnStop = new JButton("Stop");
         
-        //create color combobox and label for it
-        colorLabel = new JLabel("<html>Select Shape<br>Fill Color:</html>");
-        colors = new JComboBox( colorMenu );
-        
-        font = new JComboBox(fontSize);
+        jbColorChooser = new JButton("Select Fill Color");
         
         //create checkbox
         fillCheckBox = new JCheckBox( "Filled" );
-        
-        //create animate checkbox
-        animateCheckBox = new JCheckBox("Animate");
         
         //creates toolboxJPanel with a grid layout for canvas buttons
         toolboxPanel = new JPanel();
@@ -120,24 +95,20 @@ public class ApplicationFrame extends JFrame
         toolboxPadding.setLayout(new FlowLayout(FlowLayout.LEADING, 20, 5)); //sets padding around the edges
             
         // add widgets to widgetJPanel
-        toolboxPanel.add( undo );
-        toolboxPanel.add( redo );  
+        toolboxPanel.add(jbSave);
+        toolboxPanel.add( undo );   
         toolboxPanel.add( jbRect );
+        toolboxPanel.add(jbSquare);
         toolboxPanel.add(jbEllipse);
+        toolboxPanel.add(jbCircle);
+        toolboxPanel.add(jbTriangle); 
+        toolboxPanel.add( clear );
+        toolboxPanel.add( redo ); 
         toolboxPanel.add(jbLine);
         toolboxPanel.add(jbText);
-        toolboxPanel.add(jbCircle);
-        toolboxPanel.add( clear );
-        toolboxPanel.add(colorLabel);
-        toolboxPanel.add( colors );
+        toolboxPanel.add( jbColorChooser );
         toolboxPanel.add( fillCheckBox );
-        toolboxPanel.add(animateCheckBox);
-        toolboxPanel.add(selectShape);
-        toolboxPanel.add(jbSquare);
-        toolboxPanel.add(jbTriangle);
-        toolboxPanel.add(btnStart);
-        toolboxPanel.add(btnStop);
-        toolboxPanel.add(font);
+       
         
         // add toolbox to its padding panel
         toolboxPadding.add( toolboxPanel );
@@ -157,19 +128,26 @@ public class ApplicationFrame extends JFrame
         jbText.addActionListener( buttonHandler );
         jbSquare.addActionListener(buttonHandler);
         jbCircle.addActionListener(buttonHandler);
-        jbTriangle.addActionListener(buttonHandler);
-        btnStart.addActionListener(buttonHandler);
-        btnStop.addActionListener(buttonHandler);
+        jbTriangle.addActionListener(buttonHandler); 
+        jbSave.addActionListener(buttonHandler);
+        
+        
+        //Action Listener for color selection button to set fill color for a shape
+        jbColorChooser.addActionListener(new ActionListener() {
+        		public void actionPerformed(ActionEvent e) {
+        			c = JColorChooser.showDialog(null, "Select Fill Color", Color.BLUE);
+        			canvas.setCurrentShapeColor(c);
+        			
+        		}
+        });
+        
         
         //create handlers for color combobox and filled checkbox
         ItemListenerHandler handler = new ItemListenerHandler();
-        colors.addItemListener( handler );
         fillCheckBox.addItemListener( handler );
-        animateCheckBox.addItemListener(handler);
-        font.addItemListener(handler);
         
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        setSize( 1400, 900 );
+        setSize( 500, 500 );
         setVisible( true );
         
     } // end DrawFrame constructor
@@ -211,14 +189,10 @@ public class ApplicationFrame extends JFrame
             }  
             else if (event.getActionCommand().equals("Triangle")){
                 canvas.setCurrentShapeType(6);
-            }
-            else if(event.getActionCommand().equals("Start")) {
-            		canvas.stop = false;
-            		canvas.animateShapes();
-            }
-            else if(event.getActionCommand().equals("Stop")) {
-            		canvas.stop = true;
-            }
+            }  
+            else if (event.getActionCommand().equals("Save")){
+                savePaint();
+            } 
              
         } // end method actionPerformed
     } // end private inner class ButtonHandler
@@ -236,31 +210,24 @@ public class ApplicationFrame extends JFrame
                 boolean checkFill=fillCheckBox.isSelected() ? true : false; //
                 canvas.setCurrentShapeFilled(checkFill);
             }
-            
-            if(event.getSource() == animateCheckBox) 
-            {
-            		boolean checkAnimate = animateCheckBox.isSelected() ? true : false;
-            		canvas.setCurrentShapeAnimate(checkAnimate);
-            }
-            
-            // determine whether combo box selected
-            if ( event.getStateChange() == ItemEvent.SELECTED )
-            {
-                //if event source is combo box colors pass in colorArray at index selected.
-                if ( event.getSource() == colors)
-                {
-                    canvas.setCurrentShapeColor
-                        (colorsArray[colors.getSelectedIndex()]);
-                }
-                if(event.getSource() == font) {
-                		canvas.setFontSize(Integer.parseInt(fontSize[font.getSelectedIndex()]));
-                }
-     
-            }
-            
-           
-            
+                      
         } // end method itemStateChanged
     }
+    
+
+	public void savePaint() {
+		try
+        {
+            BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = image.createGraphics();
+            canvas.paint(graphics2D);
+            ImageIO.write(image,"jpeg", new File("/Users/aleishanelson/Desktop/SavePaintTest.jpeg"));
+        }
+        catch(Exception exception)
+        {
+            System.out.println(exception);
+        }
+		
+	}
     
 } // end class DrawFrame
--- iPaintApp/src/CanvasPanel.java
+++ iPaintApp/src/CanvasPanel.java
@@ -1,13 +1,9 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.Timer;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.event.MouseEvent;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.util.ArrayList;
 
@@ -15,16 +11,12 @@ public class CanvasPanel extends JPanel
 {
     private LinkedList<Shape> myShapes; //dynamic stack of shapes
     private LinkedList<Shape> clearedShapes; //dynamic stack of cleared shapes from undo
-    private LinkedList<Shape> animatedShapes;  //dynamic stack of animated shapes
     
     //current Shape variables
     private int currentShapeType; //0 for line, 1 for rect, 2 for oval
     private Shape currentShapeObject; //stores the current shape object
     private Color currentShapeColor; //current shape color
     private boolean currentShapeFilled; //determine whether shape is filled or not
-    private boolean currentShapeAnimated; //determine whether shape is animated or not
-    public boolean stop = false;
-    private int fontSize = 10;
     
     JLabel statusLabel; //status label for mouse coordinates
     
@@ -38,17 +30,16 @@ public class CanvasPanel extends JPanel
         
         myShapes = new LinkedList<Shape>(); //initialize myShapes dynamic stack
         clearedShapes = new LinkedList<Shape>(); //initialize clearedShapes dynamic stack
-        animatedShapes = new LinkedList<Shape>(); //initialize animatedShapes dynamic stack
+        
         //Initialize current Shape variables
-        //currentShapeType=1;
+        currentShapeType=1;
         currentShapeObject=null;
         currentShapeColor=Color.BLACK;
         currentShapeFilled=false;
-        currentShapeAnimated = false;
         
         this.statusLabel = statusLabel; //Initialize statusLabel
         
-        setLayout(null); //sets layout to border layout; default is flow layout
+        setLayout(new BorderLayout()); //sets layout to border layout; default is flow layout
         setBackground( Color.WHITE ); //sets background color of panel to white
         add( statusLabel, BorderLayout.SOUTH );  //adds a statuslabel to the south border
         
@@ -58,147 +49,69 @@ public class CanvasPanel extends JPanel
         addMouseMotionListener( handler ); 
     }
     
-    /**
-     * Calls the draw method for the existing shapes.
-     */
+    //PaintComponent method() to call the draw() method for shapes
     public void paintComponent( Graphics g )
     {
         super.paintComponent( g );
         
         // draw the shapes
         ArrayList<Shape> shapeArray=myShapes.getArray();
-        for ( int counter=shapeArray.size()-1; counter>=0; counter-- ) {
-        	shapeArray.get(counter).draw(g);
-        }
+        for ( int counter=shapeArray.size()-1; counter>=0; counter-- )
+           shapeArray.get(counter).draw(g);
         
         //draws the current Shape Object if it is not null
         if (currentShapeObject!=null)
             currentShapeObject.draw(g);
     }
     
-    public void animateShapes() {
-    		
-    		ArrayList<Shape> shapeArray = animatedShapes.getArray();
-    		for(int counter = shapeArray.size() - 1; counter >= 0; counter--) {
-    			Thread shapeThread = new Thread(new Animation());
-    			Shape currentShape = shapeArray.get(counter);
-    			int originalX1 = shapeArray.get(counter).getX1();
-    			int originalX2 = shapeArray.get(counter).getX2();
-    			//int animateX1 = originalX1;
-    			//int animatex2 = originalX2;
-    			Timer time = new Timer(5, (ActionListener) new ActionListener() {
-    				public void actionPerformed(ActionEvent e) {
-    					animate(originalX1, originalX2, currentShape);
-    				}
-    			});
-    			time.start();
-    			shapeThread.run();
-    		}
-    }
     
-    public void animate(int originalX1, int originalX2, Shape shape) {
-    		int X1 = shape.getX1();
-    		int X2 = shape.getX2();
-    		int vel = 2;
-    		if(!stop) {
-    			if(X1 < originalX1 || X2 > 1350) {
-    				shape.reverse = !shape.reverse;
-    				//vel = -vel;
-    			}
-    			if(shape.reverse) {
-    				X1 = X1 - vel;
-        			X2 = X2 - vel;
-    			}
-    			else {
-    				X1 = X1 + vel;
-    				X2 = X2 + vel;
-    			}
-    			shape.setX1(X1);
-    			shape.setX2(X2);
-    			repaint();
-    		}
-    		else {
-    			shape.setX1(originalX1);
-    			shape.setX2(originalX2);
-    			repaint();
-    		}
-    		
-    }
     
-    //Mutator methods for currentShapeType, currentShapeColor and currentShapeFilled
-    
-    /**
-     * Sets the currentShapeType to type (0 for line, 1 for rect, 2 for oval) passed in.
-     */
+    //MUTATORS  for currentShapeType, currentShapeColor and currentShapeFilled
+
+    //Sets the currentShapeType variable to type 
+    //(0 for Line, 1 for Rect, 2 for Oval, 3 for ... and so on according to shape array)
     public void setCurrentShapeType(int type)
     {
         currentShapeType=type;
     }
     
-    /**
-     * Sets the currentShapeColor to the Color object passed in.
-     * The Color object contains the color for the current shape.
-     */
+
+    //method to set the CurrentShapeColor variable to the color that is passed in from the JColorChooser
     public void setCurrentShapeColor(Color color)
     {
         currentShapeColor=color;
     }
-    
-    public void setFontSize(int size) {
-    		fontSize = size;
-    }
-    /**
-     * Sets the boolean currentShapeFilled to boolean filled passed in. 
-     * If filled=true, current shape is filled. 
-     * If filled=false, current shape is not filled.
-     */
+
+    //method to set booled 'filled' variable to true if shape should be filled with color, or false if it shouldn't.
     public void setCurrentShapeFilled(boolean filled)
     {
         currentShapeFilled=filled;
     }
     
-    public void setCurrentShapeAnimate(boolean animated)
-    {
-    		currentShapeAnimated = animated;
-    }
-    
-    
-    /**
-     * Clear the last shape drawn and calls repaint() to redraw the panel if clearedShapes is not empty
-     */
+   
+    //method to clear the last shape drawn, it calls the repatin() method to redraw the panel without that shape  
     public void clearLastShape()
     {
         if (! myShapes.isEmpty())
         {
-        	if(myShapes.getFront().getData() instanceof TextBox) {
-        			System.out.println(((TextBox)myShapes.getFront().getData()).getTextBox());
-        			ApplicationFrame.canvas.remove(((TextBox)myShapes.getFront().getData()).getTextBox());
-        	}
-        		ApplicationFrame.canvas.revalidate();
             clearedShapes.addFront(myShapes.removeFront());
-            System.out.println(myShapes.getArray());
             repaint();
         }
     }
     
-    /**
-     * Redo the last shape cleared if clearedShapes is not empty
-     * It calls repaint() to redraw the panel.
-     */
+
+    //method to repaint() the last shape that was cleared from the canvas. upon clicking it redraws the panel
     public void redoLastShape()
     {
         if (! clearedShapes.isEmpty())
         {
             myShapes.addFront(clearedShapes.removeFront());
-            System.out.println(myShapes.getArray());
             repaint();
         }
     }
     
-    /**
-     * Remove all shapes in current drawing. Also makes clearedShapes empty since you cannot redo after clear.
-     * It called repaint() to redraw the panel.
-     */
+
+    //method to clear the entire canvas. It empties the clearedShapes array so you cannot do "redo" after clearing it
     public void clearDrawing()
     {
         myShapes.makeEmpty();
@@ -206,15 +119,13 @@ public class CanvasPanel extends JPanel
         repaint();
     }
     
-    /**
-     * Private inner class that implements MouseAdapter and does event handling for mouse events.
-     */
+
+    //method to handle all of the mouse events
     private class MouseHandler extends MouseAdapter 
     {
-        /**
-         * When mouse is pressed draw a shape object based on type, color and filled.
-         * X1,Y1 & X2,Y2 coordinate for the drawn shape are both set to the same X & Y mouse position.
-         */
+
+    	//When mouse is pressed down, draw the shape based on the variables passed in (shapeType, color, and filled)
+    	//x1, y1, x2, y2 are the coordinates for the drawn shape and are set to the same position
         public void mousePressed( MouseEvent event )
         {
             switch (currentShapeType) //0 for line, 1 for rect, 2 for oval
@@ -231,11 +142,9 @@ public class CanvasPanel extends JPanel
                     currentShapeObject= new Ellipse( event.getX(), event.getY(), 
                                                    event.getX(), event.getY(), currentShapeColor, currentShapeFilled);
                     break;
-                case 3:
-                    currentShapeObject = new TextBox(event.getX(), event.getY(),
-                										event.getX(), event.getY(), currentShapeColor, fontSize);
-                		
-                    break; 
+                /*case 3:
+                    currentShapeObject= new Text();
+                    break; */
                 case 4:
                     currentShapeObject= new Circle( event.getX(), event.getY(), 
                                                    event.getX(), event.getY(), currentShapeColor, currentShapeFilled);
@@ -244,48 +153,39 @@ public class CanvasPanel extends JPanel
                     currentShapeObject= new Square( event.getX(), event.getY(), 
                                                    event.getX(), event.getY(), currentShapeColor, currentShapeFilled);
                     break;
-                		
                
-                    /*
+                    
                 case 6:
                     currentShapeObject= new Triangle( event.getX(), event.getY(), 
                                                    event.getX(), event.getY(), currentShapeColor, currentShapeFilled);
                     break;
-                    */
+                    
                     
             }// end switch case
         } // end method mousePressed
         
-        /**
-         * When mouse is released set currentShapeObject's x2 & y2 to mouse pos.
-         * Then addFront currentShapeObject onto the myShapes dynamic Stack 
-         * and set currentShapeObject to null [clearing current shape object since it has been drawn].
-         * Lastly, it clears all shape objects in clearedShapes [because you cannot redo after a new drawing]
-         * and calls repaint() to redraw panel.
-         */
+
+        /*
+         Method to handle events that should happen when the mouse is released
+        
+        
+             Upon releasing the mouse, we set the current Shape's x2 and y2 coordinates to that specific mouse position
+             then we add to the front of the currentShapeObject stack and set currentShapeObject back to null (which clears the shape  object that was drawn)
+             then is clears ALL of the shape objects in 'clearedShapes" because we don't want to be able to do "redo" after drawing it. 
+             Lastly, it calls repaint() to draw the panel with the new shape  
+        */
+        
         public void mouseReleased( MouseEvent event )
         {
-        
-        	
             //sets currentShapeObject x2 & Y2
             currentShapeObject.setX2(event.getX());
             currentShapeObject.setY2(event.getY());
             
-	if(currentShapeObject != null && currentShapeType == 3) {
-        		ApplicationFrame.canvas.add(((TextBox)currentShapeObject).getTextBox());
-        		
-        	}
             myShapes.addFront(currentShapeObject); //addFront currentShapeObject onto myShapes
-            System.out.println(myShapes.getArray());
-            
-            if(currentShapeAnimated) {
-            		animatedShapes.addFront(currentShapeObject);
-            }
             
             currentShapeObject=null; //sets currentShapeObject to null
             clearedShapes.makeEmpty(); //clears clearedShapes
             repaint();
-        	
             
         } // end method mouseReleased
         
@@ -304,7 +204,6 @@ public class CanvasPanel extends JPanel
          */
         public void mouseDragged( MouseEvent event )
         {
-        	if(currentShapeObject!=null) {
             //sets currentShapeObject x2 & Y2
             currentShapeObject.setX2(event.getX());
             currentShapeObject.setY2(event.getY());
@@ -313,7 +212,6 @@ public class CanvasPanel extends JPanel
             statusLabel.setText(String.format("Mouse Coordinates X: %d Y: %d",event.getX(),event.getY()));
             
             repaint();
-        	}
             
         } // end method mouseDragged
         
